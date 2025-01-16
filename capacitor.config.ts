600c5fec65b98add940ddc25272066499101c119
B) <22089@portalcedi.edu.mx>
2025-01-16 08:36:39 -0600

    1st commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Repaso',
+  webDir: 'www'
+};
+
+export default config;
